38e3f9049935d7a1ff13357cf27357d61b5a75b5
James York <kroysemaj@gmail.com>
2022-03-12 04:59:07 -0500

    added options to dropdowns

---
+++ src/components/helpers/sleepScoreDropdownOptions.js
@@ -0,0 +1,194 @@
+export const sleepScoreDropdownOptions = [
+  {
+    sleepIncrements: 0,
+    text: '0 minutes',
+  },
+  {
+    sleepIncrements: 1,
+    text: '30 minutes',
+  },
+  {
+    sleepIncrements: 2,
+    text: '1 hour',
+  },
+  {
+    sleepIncrements: 3,
+    text: '1 hour, 30 minutes',
+  },
+  {
+    sleepIncrements: 4,
+    text: '2 hours',
+  },
+  {
+    sleepIncrements: 5,
+    text: '2 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 6,
+    text: '3 hours',
+  },
+  {
+    sleepIncrements: 7,
+    text: '3 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 8,
+    text: '4 hours',
+  },
+  {
+    sleepIncrements: 9,
+    text: '4 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 10,
+    text: '5 hours',
+  },
+  {
+    sleepIncrements: 11,
+    text: '5 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 12,
+    text: '6 hours',
+  },
+  {
+    sleepIncrements: 13,
+    text: '6 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 14,
+    text: '7 hours',
+  },
+  {
+    sleepIncrements: 15,
+    text: '7 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 16,
+    text: '8 hours',
+  },
+  {
+    sleepIncrements: 17,
+    text: '8 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 18,
+    text: '9 hours',
+  },
+  {
+    sleepIncrements: 19,
+    text: '9 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 20,
+    text: '10 hours',
+  },
+  {
+    sleepIncrements: 21,
+    text: '10 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 22,
+    text: '11 hours',
+  },
+  {
+    sleepIncrements: 23,
+    text: '11 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 24,
+    text: '12 hours',
+  },
+  {
+    sleepIncrements: 25,
+    text: '12 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 26,
+    text: '13 hours',
+  },
+  {
+    sleepIncrements: 27,
+    text: '13 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 28,
+    text: '14 hours',
+  },
+  {
+    sleepIncrements: 29,
+    text: '14 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 30,
+    text: '15 hours',
+  },
+  {
+    sleepIncrements: 31,
+    text: '15 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 32,
+    text: '16 hours',
+  },
+  {
+    sleepIncrements: 33,
+    text: '16 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 34,
+    text: '17 hours',
+  },
+  {
+    sleepIncrements: 35,
+    text: '17 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 36,
+    text: '18 hours',
+  },
+  {
+    sleepIncrements: 37,
+    text: '18 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 38,
+    text: '19 hours',
+  },
+  {
+    sleepIncrements: 39,
+    text: '19 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 40,
+    text: '20 hours',
+  },
+  {
+    sleepIncrements: 41,
+    text: '20 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 42,
+    text: '21 hours',
+  },
+  {
+    sleepIncrements: 43,
+    text: '21 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 44,
+    text: '22 hours',
+  },
+  {
+    sleepIncrements: 45,
+    text: '22 hours, 30 minutes',
+  },
+  {
+    sleepIncrements: 46,
+    text: '23 hours',
+  },
+  {
+    sleepIncrements: 47,
+    text: '23 hours, 30 minutes',
+  },
+];

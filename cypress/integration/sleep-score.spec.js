b3057b753b8a432b43917221e5dd0d11ff2f20f5
James York <kroysemaj@gmail.com>
2022-03-15 16:18:01 -0400

    fix up e2e test

--- cypress/integration/sleep-score.spec.js
+++ cypress/integration/sleep-score.spec.js
@@ -1,13 +1,12 @@
 describe('Saving sleep score', () => {
   it('return the calculated sleep score and a success message', () => {
     const sleepScore = '80';
-    const successMessage = 'Sleep Score Saved!';
 
-    cy.server({ force404: true });
+    cy.server();
 
     cy.route({
       method: 'GET',
-      url: 'https://localhost:4002/sleep-score/save',
+      url: 'https://localhost:4000/sleep-score/save',
       response: {
         status: 200,
         sleepScore: 84,
@@ -18,6 +17,7 @@ describe('Saving sleep score', () => {
     cy.visit('/');
     cy.get('#in-bed-select').select('5 hours').should('have.value', '10');
     cy.get('#asleep-select').select('4 hours').should('have.value', '8');
+    cy.get('#calc-btn').click();
     cy.contains(sleepScore);
   });
 });

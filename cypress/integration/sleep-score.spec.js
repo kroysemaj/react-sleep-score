620d59655d340640b2241283b2fb16cfb7ee5bf1
James York <kroysemaj@gmail.com>
2022-03-15 12:04:16 -0400

    update e2e test to change dropdowns

--- cypress/integration/sleep-score.spec.js
+++ cypress/integration/sleep-score.spec.js
@@ -1,6 +1,6 @@
 describe('Saving sleep score', () => {
   it('return the calculated sleep score and a success message', () => {
-    const sleepScore = '84';
+    const sleepScore = '80';
     const successMessage = 'Sleep Score Saved!';
 
     cy.server({ force404: true });
@@ -10,12 +10,14 @@ describe('Saving sleep score', () => {
       url: 'https://localhost:4002/sleep-score/save',
       response: {
         status: 200,
+        sleepScore: 84,
         status_message: 'Sleep Score Saved!',
       },
     });
 
-    cy.visit('/sleep-score/save');
+    cy.visit('/');
+    cy.get('#in-bed-select').select('5 hours').should('have.value', '10');
+    cy.get('#asleep-select').select('4 hours').should('have.value', '8');
     cy.contains(sleepScore);
-    cy.contains(successMessage);
   });
 });

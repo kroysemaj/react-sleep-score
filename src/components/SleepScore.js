13e66a56d53e77de63229bca066f1795bda0aad6
James York <kroysemaj@gmail.com>
2022-03-14 23:50:30 -0400

    connect sleep score reducer to ui

--- src/components/SleepScore.js
+++ src/components/SleepScore.js
@@ -1,12 +1,25 @@
+import { useReducer } from 'react';
 import SleepScoreForm from './SleepScoreForm';
 import SleepScoreResult from './SleepScoreResult';
+import {
+  sleepScoreReducer,
+  updateDurationAsleep,
+  updateDurationInBed,
+} from '../store/sleepScoreReducer';
 
-const SleepScore = () => (
-  <div>
-    <h1>Let's score your latest sleep!</h1>
-    <SleepScoreForm />
-    <SleepScoreResult />
-  </div>
-);
+const SleepScore = () => {
+  const [state, dispatch] = useReducer(sleepScoreReducer, sleepScoreReducer());
+  return (
+    <div>
+      <h1>Let's score your latest sleep!</h1>
+      <SleepScoreForm
+        scores={state}
+        updateDurationAsleep={() => dispatch(updateDurationAsleep())}
+        updateDurationInBed={() => dispatch(updateDurationInBed())}
+      />
+      <SleepScoreResult result={state} />
+    </div>
+  );
+};
 
 export default SleepScore;

4ce564917c7aa690d961887eb8e3b57e61f0faa9
James York <kroysemaj@gmail.com>
2022-03-15 12:06:36 -0400

    update tests to match new state object

--- src/store/__tests__/sleepScoreReducer.spec.js
+++ src/store/__tests__/sleepScoreReducer.spec.js
@@ -1,29 +1,78 @@
+/**
+ * @jest-environment node
+ */
 import {
   sleepScoreReducer,
   updateDurationInBed,
   updateDurationAsleep,
+  processSleepScore,
+  publishSleepScore,
   initialState,
 } from '../sleepScoreReducer';
 
 describe('Sleep score reducer', () => {
   it('should return a valid initial state', () => {
     expect(sleepScoreReducer()).toEqual({
-      durationInBed: -1,
-      durationAsleep: -1,
+      durationInBed: '',
+      durationAsleep: '',
+      sleepScore: -1,
     });
   });
 
   it('should update duration in bed property', () => {
     expect(sleepScoreReducer(initialState, updateDurationInBed(3))).toEqual({
       durationInBed: 3,
-      durationAsleep: -1,
+      durationAsleep: '',
+      sleepScore: -1,
     });
   });
 
   it('should update duration in bed property', () => {
     expect(sleepScoreReducer(initialState, updateDurationAsleep(5))).toEqual({
-      durationInBed: -1,
+      durationInBed: '',
       durationAsleep: 5,
+      sleepScore: -1,
+    });
+  });
+
+  it('should update the sleep score property', () => {
+    expect(
+      sleepScoreReducer(
+        {
+          durationInBed: 10,
+          durationAsleep: 6,
+          sleepScore: -1,
+        },
+        publishSleepScore(60),
+      ),
+    ).toEqual({
+      durationInBed: 10,
+      durationAsleep: 6,
+      sleepScore: 60,
+    });
+  });
+
+  it('should derive and save the sleep score', async () => {
+    const saveSleepScoreResponse = {
+      status: 200,
+      sleepScore: 90,
+      status_message: 'Sleep Score Saved!',
+    };
+
+    const api = {
+      saveSleepScore: () => Promise.resolve(saveSleepScoreResponse),
+    };
+
+    const expectedState = await processSleepScore(
+      api,
+      { durationInBed: 10, durationAsleep: 9 },
+      90,
+    );
+
+    expect(expectedState).toEqual({
+      durationInBed: 10,
+      durationAsleep: 9,
+      sleepScore: 90,
     });
   });
 });

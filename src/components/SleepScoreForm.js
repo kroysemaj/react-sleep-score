bf675a4d062d24636ca72da4ec6ed4c7953d8ad2
James York <kroysemaj@gmail.com>
2022-03-15 12:06:17 -0400

    Add click handler for calc button

--- src/components/SleepScoreForm.js
+++ src/components/SleepScoreForm.js
@@ -1,18 +1,28 @@
 import DurationSelect from './DurationSelect';
+import api from '../api';
 
 const SleepScoreForm = ({
   scores,
   updateDurationInBed,
   updateDurationAsleep,
+  processSleepScore,
 }) => {
   const formIsValid =
     scores.durationAsleep === '' || scores.durationInBed === '';
+
+  const handleClick = e => {
+    e.preventDefault();
+    const sleepScore = 100 * (scores.durationInBed / scores.durationAsleep);
+    processSleepScore(api, scores, sleepScore);
+  };
 
   return (
     <form>
       <DurationSelect location="in-bed" handleSelect={updateDurationInBed} />
       <DurationSelect location="asleep" handleSelect={updateDurationAsleep} />
-      <button disabled={formIsValid}>Calculate</button>
+      <button disabled={formIsValid} onClick={handleClick}>
+        Calculate
+      </button>
     </form>
   );
 };
--- src/store/sleepScoreReducer.js
+++ src/store/sleepScoreReducer.js
@@ -1,27 +1,45 @@
 const initialState = {
   durationInBed: '',
   durationAsleep: '',
+  sleepScore: -1,
 };
 
 const UPDATE_DURATION_IN_BED = 'UPDATE_DURATION_IN_BED';
 const UPDATE_DURATION_ASLEEP = 'UPDATE_DURATION_ASLEEP';
+const PUBLISH_SLEEP_SCORE = 'PUBLISH_SLEEP_SCORE';
 
-export const updateDurationInBed = duration => ({
-  payload: duration,
-  type: UPDATE_DURATION_IN_BED,
-});
+export const updateDurationInBed = duration => {
+  console.log('#updateDurationInBed', duration);
+  return {
+    payload: duration,
+    type: UPDATE_DURATION_IN_BED,
+  };
+};
 
 export const updateDurationAsleep = duration => ({
   payload: duration,
   type: UPDATE_DURATION_ASLEEP,
 });
 
+export const processSleepScore = (api, state, score) =>
+  api.saveSleepScore(score).then(res => {
+    return sleepScoreReducer(state, publishSleepScore(res.sleepScore));
+  });
+
+export const publishSleepScore = sleepScore => ({
+  payload: sleepScore,
+  type: PUBLISH_SLEEP_SCORE,
+});
+
 export const sleepScoreReducer = (state = initialState, action = {}) => {
+  console.log('reducer action:', action);
   switch (action.type) {
     case UPDATE_DURATION_IN_BED:
       return { ...state, durationInBed: action.payload };
     case UPDATE_DURATION_ASLEEP:
       return { ...state, durationAsleep: action.payload };
+    case PUBLISH_SLEEP_SCORE:
+      return { ...state, sleepScore: action.payload };
     default:
       return state;
   }

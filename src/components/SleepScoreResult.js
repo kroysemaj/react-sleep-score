e1ab20ea4f18c8dcecac8cf182fdec57b8bc5f41
James York <kroysemaj@gmail.com>
2022-03-16 17:29:56 -0400

    style results element

--- src/components/SleepScoreResult.js
+++ src/components/SleepScoreResult.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
+import { Box, Typography } from '@material-ui/core';
 
 const SleepScoreResult = ({ result }) => (
-  <p>
-    Calculated Sleep Score: <span>{result !== -1 ? result : ''}</span>
-  </p>
+  <Box>
+    <Typography variant="h6">Calculated Sleep Score:</Typography>
+    <Typography variant="h5" color={result > 50 ? 'primary' : 'error'}>
+      {result !== -1 ? result : ''}
+    </Typography>
+  </Box>
 );
 
 const mapStateToProps = state => ({
-  result: state.sleepScore.score,
+  result: state.sleepScore.sleepScore.score,
 });
 
 export default connect(mapStateToProps)(SleepScoreResult);
